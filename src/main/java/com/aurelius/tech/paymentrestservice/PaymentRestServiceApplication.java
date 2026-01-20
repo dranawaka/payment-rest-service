b3e12cf8df4188d3e3076276d78efb97aa0fbb84
Dilan <dilan.hansike@gmail.com>
2026-01-20 14:52:53 -0500

    init

---
+++ src/main/java/com/aurelius/tech/paymentrestservice/PaymentRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aurelius.tech.paymentrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentRestServiceApplication.class, args);
+    }
+
+}
